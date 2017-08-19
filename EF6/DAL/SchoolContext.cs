7053568cb8f47b8db0178c9570b8ea3c9b26a814
Owen <owen2862@live.com>
2017-08-19 22:53:57 +0800

    加入專案檔案。

---
+++ EF6/DAL/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using EF6.Models;
+
+namespace EF6.DAL {
+    public class SchoolContext : DbContext {
+
+        public SchoolContext() : base("SchoolContext") {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            //避免table變成複數
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+
+}
