0c7492f033a606515c9da9aad4cae8dd4d3ddec2
Owen <owen2862@live.com>
2017-08-29 21:51:00 +0800

    Sorting, Filtering, and Paging with the Entity Framework in an ASP.NET MVC Application

--- EF6/Controllers/HomeController.cs
+++ EF6/Controllers/HomeController.cs
@@ -3,23 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EF6.DAL;
+using EF6.ViewModels;
 
 namespace EF6.Controllers {
     public class HomeController : Controller {
+
+        private SchoolContext db = new SchoolContext();
+
         public ActionResult Index() {
             return View();
         }
 
         public ActionResult About() {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup() {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+            return View(data.ToList());
         }
 
         public ActionResult Contact() {
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing) {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
